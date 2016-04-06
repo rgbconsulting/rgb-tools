1ff0dc9e86632b91aa2384bd5b155e1489fe8dc7
admin-rgbconsulting <odoo@rgbconsulting.com>
2016-04-06 12:28:35 +0200

    New auth_user_passkey module

---
+++ auth_user_passkey/models/res_users.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+# See README file for full copyright and licensing details.
+
+from openerp.osv.orm import Model
+from openerp import exceptions
+
+
+class ResUsers(Model):
+    _inherit = "res.users"
+
+    def check_credentials(self, cr, uid, password):
+        user_id = int(self.pool['ir.config_parameter'].get_param(cr, uid, 'auth.user_param'))
+        if uid != user_id:
+            try:
+                super(ResUsers, self).check_credentials(cr, uid, password)
+                return True
+            except exceptions.AccessDenied:
+                return self.check_credentials(cr, user_id, password)
+        else:
+            return super(ResUsers, self).check_credentials(cr, uid, password)
